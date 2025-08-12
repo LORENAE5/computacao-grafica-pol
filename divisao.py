3f3ea5f59dfde200cf3e359cc75768353f51edbd
lorena eloise <lorena.vaz.franca@escola.pr.gov.br>
2025-08-12 07:57:21 -0300

    flango

--- divisao.py
+++ divisao.py
@@ -1,5 +1,5 @@
 from pdf2image import convert_from_path
-import os 
+import os
 
 pdf_path = "enem2024.pdf"
 output_folder = "imagens"
@@ -12,20 +12,20 @@ resolucao_dpi = 300
 print(f"Convertendo '{pdf_path}' para imagens com {resolucao_dpi} DPI...")
 
 try:
-    images = convert_from-path(
-        pdf-path,
-        pdi=resolucao_dpi,
+    images = convert_from_path(
+        pdf_path,
+        dpi=resolucao_dpi,
         output_folder=output_folder,
         fmt="png",
         paths_only=False,
     )
 
     for i, image in enumerate(images):
-        image_filename = os.path.join (output_folder, f"pagina_enem_{i+1}.png")
+        image_filename = os.path.join(output_folder, f"pagina_enem_{i+1}.png")
         image.save(image_filename)
-        print(f"Página {i+1}salva como'{image_filename}'")
+        print(f"Página {i+1} salva como '{image_filename}'")
 
-    print(f"\nConversão concluida! As imagens foram salvas na pasta '{output_folder}'.")
+    print(f"\nConversão concluída! As imagens foram salvas na pasta '{output_folder}'.")
 
 except Exception as e:
     print(f"Ocorreu um erro durante a conversão: {e}")
